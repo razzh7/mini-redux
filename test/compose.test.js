21492b0f8c2109f5f7bb20b869c211508a901da1
razzh <razzhavenir@163.com>
2022-12-16 12:52:49 +0800

    test: add compose

---
+++ test/compose.test.js
@@ -0,0 +1,13 @@
+import { describe, test, expect, vi } from 'vitest'
+import { compose } from '../src/mini-redux/compose'
+
+describe('compose', () => {
+  test('composes from right to left', () => {
+    const double = (x) => x * 2
+    const square = (x) => x * x
+
+    expect(compose(square)(5)).toBe(25)
+    expect(compose(square, double)(5)).toBe(100)
+    expect(compose(double, square, double)(5)).toBe(200)
+  })
+})
