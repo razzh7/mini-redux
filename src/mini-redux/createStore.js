93c9fac3f185387e5f6e4a969f442c46a83c3a6d
razzh <razzhavenir@163.com>
2022-12-16 12:59:41 +0800

    style: 添加注释

--- src/mini-redux/createStore.js
+++ src/mini-redux/createStore.js
@@ -1,19 +1,22 @@
 export default function createStore(reducer, preloadedState, enhancer) {
   let state = preloadedState
+  // 订阅器
   const listeners = []
-
+  // 增强器 enhancer
   if (enhancer !== undefined && typeof enhancer === 'function') {
     return enhancer(createStore)(reducer, preloadedState)
   }
 
+  // 获取最新的 State 值
   function getState() {
     return state
   }
 
-  // 订阅
+  // 订阅函数
   function subscribe(listener) {
     listeners.push(listener)
 
+    // 解绑订阅函数
     return function unsubscribe() {
       const index = listeners.indexOf(listener)
       listeners.splice(index, 1)
@@ -22,8 +25,10 @@ export default function createStore(reducer, preloadedState, enhancer) {
 
   // 发布订阅
   function dispatch(action) {
+    // 调用 reducer 将最新的值赋值给 state
     state = reducer(state, action)
 
+    // 循环执行订阅器里面的函数
     for (let i = 0; i < listeners.length; i++) {
       const listener = listeners[i]
       listener()
@@ -32,6 +37,7 @@ export default function createStore(reducer, preloadedState, enhancer) {
     return action
   }
 
+  // 当一个 Store 创建时，自动初始化 state 的值
   dispatch({ type: '@@redux/INIT' })
 
   return {
